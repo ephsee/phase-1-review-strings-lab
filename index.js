b184770e56463c06c644de73a9680221fc8ac9f0
Francois Spies <fcspies@gmail.com>
2022-01-09 18:45:45 -0500

    commit tree 97f77609d2dc2dd6b7c37ea88a8ac84f05d30393

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1)
+
+// const shortGreeting = `Welcome, ${firstInitial}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
